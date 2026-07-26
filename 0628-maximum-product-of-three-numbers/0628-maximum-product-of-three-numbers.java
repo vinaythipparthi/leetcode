40d88f728c01f6bd31d101201a6e31682eb38b33
Vinay <vinaythipparthi99@gmail.com>
2026-07-26 16:34:46 +0530

    Time: 2 ms (99.61%), Space: 47.1 MB (97.34%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,9 +1,38 @@
 class Solution {
     public int maximumProduct(int[] nums) {
         int n = nums.length;
-        Arrays.sort(nums);
-        int max1 = nums[n-1]*nums[n-2]*nums[n-3];
-        int max2 = nums[0]*nums[1]*nums[n-1];
-        return Math.max(max1, max2);
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        //
+        for(int i=0;i<n;i++){
+            int val = nums[i];
+
+            //find maxes
+            if(max1 <= val){
+                max3 = max2;
+                max2 = max1;
+                max1 = val;
+            }else if(max2 <= val){
+                max3 = max2;
+                max2 = val;
+            }else if(max3 < val){
+                max3 = val;
+            }
+
+            //find mins
+            if(min1 >= val){
+                min2 = min1;
+                min1 = val;
+            }else if(min2 > val){
+                min2 = val;
+            }
+        }
+        //Find max of min&max
+        int ans = Math.max(max1*max2*max3, min1*min2*max1);
+        return ans;
     }
 }
