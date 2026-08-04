1a0ec64a91297b678e7380a75b407d8e7e9b71c1
Vinay <vinaythipparthi99@gmail.com>
2026-08-04 12:12:21 +0530

    Time: 1 ms (100%), Space: 46.3 MB (98.93%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,37 +1,23 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
+        //frequency check
         int n = nums.length;
-        Arrays.sort(nums);
-        int i=0;
-        int j=n-1;
-        boolean flag1 = true;
-        boolean flag2 = true;
-        while(i<=j){
-            if(!flag1 && !flag2)  break;
-            if(nums[i]+1 == nums[i+1] && flag1){
-                i++;
-            }else{
-                flag1 = false;
-            }
-            if(nums[j]-1 == nums[j-1] && flag2){
-                j--;
-            }else{
-                flag2 = false;
+        int min = nums[0];
+        int max = nums[0];
+        for(int i:nums){
+            min = Math.min(min,i);
+            max = Math.max(max,i);
+        }
+        int[] arr = new int[max+1];
+        for(int i=0;i<n;i++){
+            arr[nums[i]]++;
+        }
+        List<Integer> li = new ArrayList<>();
+        for(int i=min;i<max;i++){
+            if(arr[i] == 0){
+                li.add(i);
             }
         }
-        //
-        List<Integer> ans = new ArrayList<>();
-        for(int x=nums[i]+1;x<=nums[j]-1;x++){
-            boolean flag = false;
-            for(int num=i+1;num<=j-1;num++){
-                if(nums[num] == x){
-                    flag = true;
-                    break;
-                }
-            }
-            if(!flag)    ans.add(x);
-        }
-        return ans;
-
+        return li;
     }
 }
