b81b499ede802715828b1fb53413a01f73e8d8b8
Vinay <vinaythipparthi99@gmail.com>
2026-08-06 18:59:19 +0530

    Time: 0 ms (100%), Space: 46.7 MB (31%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -14,6 +14,7 @@ public class Solution {
         if(head==null || head.next==null){
             return null;
         }
+        
         ListNode fast = head;
         ListNode slow = head;
         while(fast!=null && fast.next!=null){
