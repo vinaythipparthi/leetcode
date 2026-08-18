288b5805e7b0738f702519317a3c3f604b0f284a
Vinay <vinaythipparthi99@gmail.com>
2026-08-18 12:27:19 +0530

    Time: 1 ms (99.3%), Space: 44.9 MB (69.12%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,24 +1,27 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
         int n = nums.length;
-        for(int i=0;i<=n-k;i++){
-            Set<Integer> set = new HashSet<>();
-            for(int j=i;j<i+k && j<n;j++){
-                int val = nums[j];
-                set.add(val);
-            }
-            for(int val:set){
-                map.put(val, map.getOrDefault(val,0)+1);
-            }
-        }
-        //
-        int ans = -1;
-        for(int val:map.keySet()){
-            if(map.get(val) == 1){
+        if(k==n){
+            int ans = -1;
+            for(int val:nums){
                 ans = Math.max(ans, val);
             }
+            return ans;
         }
-        return ans;
+        int[] freq = new int[51];
+        for(int val:nums){
+            freq[val]++;
+        }
+        if(k==1){
+            //find largest which occurs once
+            for(int i=50;i>0;i--){
+                if(freq[i] == 1)    return i;
+            }
+            return -1;
+        }
+        int res = -1;
+        if(freq[nums[0]] == 1)    res = Math.max(res, nums[0]);
+        if(freq[nums[n-1]] == 1)    res = Math.max(res, nums[n-1]);
+        return res;
     }
 }
