5c795a75af5833b9784cedd8fe223d491f296f80
Vinay <vinaythipparthi99@gmail.com>
2026-09-02 11:15:02 +0530

    Time: 0 ms (100%), Space: 45.2 MB (35.83%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
+        //if even a single element in nums1 is odd then nums2 can have all odd elements
+        //because odd - even = odd & odd - odd = odd.
+        //if not then all even
+        //both are true
         return true;
     }
 }
