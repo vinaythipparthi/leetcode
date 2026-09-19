5f34ba059ab596f8a3642361775e3a45abe89f0e
Vinay <vinaythipparthi99@gmail.com>
2026-09-19 08:54:37 +0530

    Time: 38 ms (50.54%), Space: 46.9 MB (27.55%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.java
+++ 0935-knight-dialer/0935-knight-dialer.java
@@ -1,34 +1,35 @@
 class Solution {
-    int m = (int)Math.pow(10,9) + 7;
-    int[][] adj = {
+    int mod = (int)Math.pow(10,9)+7;
+    int[][] keypad = {
         {4,6},
-        {8,6},
+        {6,8},
         {7,9},
         {4,8},
-        {3,9,0},
+        {0,3,9},
         {},
-        {1,7,0},
+        {0,1,7},
         {2,6},
         {1,3},
         {2,4}
     };
     public int knightDialer(int n) {
+        Integer[][] dp = new Integer[10][n+1];
         int result = 0;
-        Integer[][] dp = new Integer[n+1][10];
         for(int i=0;i<=9;i++){
-            result = (result + solve(n-1, i, dp)) % m;
+            result = (result+solve(i,n-1,dp)) % mod;
         }
         return result;
     }
-    public int solve(int n, int cell, Integer[][] dp){
-        if(n==0)    return 1;
-
-        if(dp[n][cell] != null) return dp[n][cell];
-        int ans = 0;
-        for(int i=0;i<adj[cell].length;i++){
-            int val = adj[cell][i];
-            ans = (ans + solve(n-1, val, dp)) % m;
+    public int solve(int cell, int n, Integer[][] dp){
+        if(n==0){
+            return 1;
         }
-        return dp[n][cell] = ans;
+
+        if(dp[cell][n] != null) return dp[cell][n];
+        int ans = 0;
+        for(int i=0;i<keypad[cell].length;i++){
+            ans = (ans + solve(keypad[cell][i],n-1,dp)) % mod;
+        }
+        return dp[cell][n] = ans;
     }
 }
